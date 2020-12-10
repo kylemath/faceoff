7366f5af76c00ad9da042fb3da14a7e437de6c13
Kyle Mathewson <kylemath@gmail.com>
2020-12-10 00:08:41 -0700

    changed canvas to animation

--- src/components/PageSwitcher/PageSwitcher.js
+++ src/components/PageSwitcher/PageSwitcher.js
@@ -27,10 +27,11 @@ const spectro = translations.types.spectro;
 const Canvas = props => {
   const canvasRef = useRef(null)
 
-  const draw = ctx => {
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     ctx.fillStyle = '#000000'
     ctx.beginPath()
-    ctx.arc(50, 100, 20, 0, 2*Math.PI)
+    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
     ctx.fill()
   }
 
@@ -53,7 +54,6 @@ const Canvas = props => {
   }, [draw])
 
   return <canvas ref={canvasRef} {...props}/>
-  console.log('ping')
 }
 
 // export default Canvas
